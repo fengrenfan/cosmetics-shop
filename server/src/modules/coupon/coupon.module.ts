eebc0327f3caa7662a8eabe9309eb1fa84781b47
冯仁凡 <fengrf@thinkive.com>
2026-04-24 15:32:26 +0800

    feat(coupon): import ScheduleModule for cron jobs

--- server/src/modules/coupon/coupon.module.ts
+++ server/src/modules/coupon/coupon.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CouponController } from './coupon.controller';
 import { CouponService } from './coupon.service';
 import { Coupon, UserCoupon } from './coupon.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coupon, UserCoupon])],
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([Coupon, UserCoupon])],
   controllers: [CouponController],
   providers: [CouponService],
   exports: [CouponService],
