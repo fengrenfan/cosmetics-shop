f2b36398d632cfa0462238258d2a46cab1b226f4
fengrenfan <fengrenfan@outlook.com>
2026-05-07 14:55:02 +0800

    修复优惠券列表页import语法错误，改用默认导入

--- miniapp/src/pages/coupon/list.vue
+++ miniapp/src/pages/coupon/list.vue
@@ -44,7 +44,7 @@
 <script setup>
 import { ref } from 'vue';
 import { onMounted } from '@dcloudio/uni-app';
-import { request } from '@/utils/request';
+import request from '@/utils/request';
 
 const availableCoupons = ref([]);
 
