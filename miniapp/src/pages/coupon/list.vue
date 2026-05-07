114e30b12ee5865b9030a9dbe945d72aa644e820
fengrenfan <fengrenfan@outlook.com>
2026-05-07 14:58:28 +0800

    修复优惠券列表页request导入方式

--- miniapp/src/pages/coupon/list.vue
+++ miniapp/src/pages/coupon/list.vue
@@ -43,7 +43,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import { request } from '@/utils/request';
+import request from '@/utils/request.js';
 
 const availableCoupons = ref([]);
 
